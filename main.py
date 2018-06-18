1a956f44f090718c542bf1ae0b300c258937007b
Daniel Watson <watsondaniel6@gmail.com>
2018-06-18 19:54:33 +0100

    WIP fixing bugs to build model graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-from os import listdir
+import os
+import random
 
 import tensorflow as tf
 from tensorflow.contrib import seq2seq
@@ -8,10 +9,11 @@ from gensim.models import KeyedVectors
 
 parser = argparse.ArgumentParser()
 
+# Hyperparameter args
 parser.add_argument('--initial_lr', type=float, default=1e-3,
   help="Initial learning rate.")
 parser.add_argument('--vocabulary_size', type=int, default=20000,
-  help="Keep only the n most common words of the training data."))
+  help="Keep only the n most common words of the training data.")
 parser.add_argument('--batch_size', type=int, default=16,
   help="Stochastic gradient descent minibatch size.")
 parser.add_argument('--hidden_size', type=int, default=512,
@@ -22,16 +24,22 @@ parser.add_argument('--sample_prob', type=float, default=.3,
   help="Decoder probability to sample from its predictions duing training.")
 parser.add_argument('--max_grad_norm', type=float, default=5.,
   help="Clip gradients to the specified maximum norm.")
+parser.add_argument('--train_embeddings', type=bool, default=False,
+  help="Set to backpropagate over the word embedding matrix.")
+parser.add_argument('--eos_token', type=bool, default=False,
+  help="Set to use the end-of-string token when running on inference.")
+parser.add_argument('--epochs', type=int, default=10,
+  help="Number of epochs to run when training the model.")
+
+# Configuration args
+parser.add_argument('--encode', type=str, default='',
+  help="Set to a file path to obtain sentence vectors for the given lines.")
 parser.add_argument('--embeddings_path', type=str, default="./word2vecModel",
   help="Path to the pre-trained word embeddings model.")
 parser.add_argument('--dataset_path', type=str, default="./books",
   help="Path to the BookCorpus dataset files.")
-parser.add_argument('--train_embeddings', type=bool, default=False,
-  help="Set to backpropagate over the word embedding matrix.")
-parser.add_argument('--encode', type=str, default='',
-  help="Set to a file path to obtain sentence vectors for the given lines.")
-parser.add_argument('--eos_token', type=bool, default=False,
-  help="Set to use the end-of-string token when running on inference.")
+parser.add_argument('--model_name', type=str, default="default",
+  help="Will save/restore model in ./output/[model_name].")
 
 FLAGS = parser.parse_args()
 
@@ -47,10 +55,10 @@ def build_encoder(inputs, seq_length):
   fw_cell = tf.nn.rnn_cell.GRUCell(FLAGS.hidden_size, name="encoder_fw")
   bw_cell = tf.nn.rnn_cell.GRUCell(FLAGS.hidden_size, name="encoder_bw")
   return tf.nn.bidirectional_dynamic_rnn(
-    fw_cell, bw_cell, inputs, sequence_length=seq_length)
+    fw_cell, bw_cell, inputs, sequence_length=seq_length, dtype=tf.float32)
 
 
-def build_decoder(thought, labels, seq_length, embedding_matrix, name_id=0):
+def build_decoder(thought, labels, embedding_matrix, name_id=0):
   """When called, adds a decoder layer to the computational graph."""
   cell = tf.nn.rnn_cell.GRUCell(FLAGS.hidden_size, name="decoder%d" % name_id)
   
@@ -60,15 +68,17 @@ def build_decoder(thought, labels, seq_length, embedding_matrix, name_id=0):
 
   # Scheduled sampling with constant probability. Labels are shifted to the
   # right by adding a start-of-string token (id: 2).
-  shifted_labels = tf.concat(
-    [tf.tile(2, FLAGS.batch_size), labels[::-1]], axis=1)
+  sos_tokens = tf.tile([[2]], [FLAGS.batch_size, 1])
+  shifted_labels = tf.concat([sos_tokens, labels[::-1]], axis=1)
+  seq_length = get_sequence_length(shifted_labels)
+  decoder_in = get_embeddings(shifted_labels)
   helper = seq2seq.ScheduledEmbeddingTrainingHelper(
-    shifted_labels, sequence_length, get_embeddings, FLAGS.sample_prob)
+    decoder_in, seq_length, get_embeddings, FLAGS.sample_prob)
   # Final layer for both decoders that converts decoder output to predictions.
-  if decoder_id > 0:
+  if name_id > 0:
      tf.get_variable_scope().reuse_variables()
   output_layer = tf.layers.Dense(FLAGS.vocabulary_size, name="output_layer")
-  decoder = seq2seqBasicDecoder(
+  decoder = seq2seq.BasicDecoder(
     cell, helper, thought, output_layer=output_layer)
   return seq2seq.dynamic_decode(decoder, impute_finished=True)
 
@@ -83,21 +93,20 @@ def build_model():
     tf.int32, shape=[FLAGS.batch_size, FLAGS.max_length], name="bw_labels")
   
   input_length = get_sequence_length(inputs)
-  fw_length = get_sequence_length(fw_labels)
-  bw_length = get_sequence_length(bw_labels)
 
   lr = tf.Variable(FLAGS.initial_lr, trainable=False, name="lr")
-  global_step = tf.Variable(0, trainable=False, name="global_step")
+  step = tf.Variable(0, trainable=False, name="global_step")
 
-  w2v_model = KeyedVectors.load_word2vec_format(FLAGS.embeddings_path)
+  w2v_model = KeyedVectors.load(FLAGS.embeddings_path, mmap='r')
   embedding_matrix = tf.Variable(
-    w2v_model.syn0, trainable=FLAGS.train_embeddings, name="embedding_matrix")
+    w2v_model.syn0[:FLAGS.vocabulary_size],
+    trainable=FLAGS.train_embeddings, name="embedding_matrix")
   embeddings = tf.nn.embedding_lookup(embedding_matrix, inputs)
   
   thought = build_encoder(embeddings, input_length)
 
-  fw_logits = build_decoder(thought, fw_labels, fw_length, embedding_matrix)
-  bw_logits = build_decoder(thought, fw_labels, bw_length, embedding_matrix,
+  fw_logits = build_decoder(thought, fw_labels, embedding_matrix)
+  bw_logits = build_decoder(thought, fw_labels, embedding_matrix,
                             name_id=1)
   
   # Mask the loss to avoid taking padding tokens into account.
@@ -111,9 +120,9 @@ def build_model():
   grads, _ = tf.clip_by_global_norm(
     tf.gradients(loss, tvars), FLAGS.max_grad_norm)
   train_op = tf.train.AdamOptimizer(lr).apply_gradients(
-    zip(grads, tvars), global_step=global_step)
+    zip(grads, tvars), global_step=step)
 
-  return thought, loss, lr, global_step, train_op
+  return inputs, fw_labels, bw_labels, thought, loss, lr, step, train_op
 
 
 def sequences(fp):
@@ -131,29 +140,75 @@ def sequences(fp):
       if len(sentence_buffer) == max_length or word == '.':
         if append_eos:
           sentence_buffer.append(3)
-        # Pad the sentence as a final step.
+        # Pad the sentence as a final step (id: 0).
         while len(sentence_buffer) < FLAGS.max_length:
           sentence_buffer.append(0)
         yield sentence_buffer
         sentence_buffer = []
 
 
-def batches(sequences):
-  for 
+def batches(seqs):
+  """Yield batches given a sequence iterable."""
+  batch_buffer = []
+  for seq in seqs:
+    batch_buffer.append(seq)
+    if len(batch_buffer) == FLAGS.batch_size:
+      yield batch_buffer
+      batch_buffer = []
+  # Check for remainder sentences and pad the rest of the batch.
+  if len(batch_buffer) > 0:
+    while len(batch_buffer) < FLAGS.batch_size:
+      batch_buffer.append([0] * FLAGS.max_length)
+    yield batch_buffer
 
 
 if __name__ == '__main__':
-  build_model()
-
-  # Inference
-  if FLAGS.encode:
-    # TODO: Restore model
-    
-    with open(FLAGS.encode) as f:
-      s
   
-  # Training
-  else:
-    for filename in listdir(FLAGS.dataset_path):
-      seqs = list(sequences())
+  print("Building computational graph...")
+  graph = tf.Graph()
+  with graph.as_default():
+    inputs, fw_l, bw_l, thought, loss, lr, step, train_op = build_model()
+
+  with tf.Session(graph=graph) as sess:
+    # Check if model can be restored
+    saver = tf.train.Saver()
+    output_dir = os.path.join('output', FLAGS.model_name)
+    ckpt = tf.train.get_checkpoint_state(output_dir)
+    if ckpt:
+      print("Restoring model...")
+      saver.restore(sess, ckpt.model_checkpoint_path)
+      print("Restored model at step", sess.run(global_step))
+    else:
+      print("Initializing model...")
+      sess.run(tf.global_variables_initializer())
+      tf.gfile.MakeDirs(output_dir)
+      print("Initialized model at", output_dir)
+
+    # Inference
+    if FLAGS.encode:
+      with open(FLAGS.encode) as fp:
+        for batch in batches(sequences(fp)):
+          encoded_batch = sess.run(thought, feed_dict={inputs: batch})
+          for v in encoded_batch:
+            # The VEC string makes the resulting lines grep-able.
+            print("VEC", *v)
+  
+    # Training
+    else:
+      for i in range(FLAGS.epochs):
+        print("Entering epoch %d..." % i)
+        file_list = os.listdir(FLAGS.dataset_path)
+        random.shuffle(file_list)
+        for filename in os.listdir(FLAGS.dataset_path):
+          with open(filename) as fp:
+            # TODO: can we find a way to not read the entire file?
+            seqs = list(sequences(fp))
+            # Since `batches` is a generator, this won't copy `seqs`
+            in_batches = batches(seqs[1:-1])
+            fw_batches = batches(seqs[2:])
+            bw_batches = batches(seqs[:-2])
+            for in_, fw, bw in zip(in_batches, fw_batches, bw_batches):
+              step_loss, _ = sess.run(
+                [loss, train_op], feed_dict={inputs: in_, fw_l: fw, bw_l: bw})
+              print("Step", sess.run(step), "(loss={0:})")
 
